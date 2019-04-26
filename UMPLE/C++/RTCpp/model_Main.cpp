69c5532f5aecbd27c92a3778237e2667d68f187f
h134036 <sellevold_92@hotmail.com>
2019-04-26 10:58:30 +0200

    Lagt til java, php, c++, sql

---
+++ UMPLE/C++/RTCpp/model_Main.cpp
@@ -0,0 +1,16 @@
+//PLEASE DO NOT EDIT THIS CODE
+//This code was generated using the UMPLE 1.29.1.4448.81a70243a modeling language
+#include <Prosjekt.h>
+#include <Arrangementdeltagelse.h>
+#include <Stemme.h>
+#include <Bruker.h>
+#include <Bedrift.h>
+#include <Brukegruppe.h>
+#include <Arrengement.h>
+#include <Stemmemetode.h>
+#include <Studie.h>
+#include <Kategori.h>
+int main(int argc, char *argv[]){
+	
+  return 0;
+}
